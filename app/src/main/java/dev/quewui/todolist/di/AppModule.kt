b684002aa8d412d4562bcf4acac5859c54b0d57f
Kaue Pereira <kauefontes@outlook.com>
2022-10-08 20:05:17 -0400

    Created AppModule Singleton

---
+++ app/src/main/java/dev/quewui/todolist/di/AppModule.kt
@@ -0,0 +1,31 @@
+package dev.quewui.todolist.di
+
+import android.app.Application
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dev.quewui.todolist.data.TodoDatabase
+import dev.quewui.todolist.data.TodoRepository
+import dev.quewui.todolist.data.TodoRepositoryImpl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideTodoDatabase(app: Application): TodoDatabase {
+        return Room.databaseBuilder(
+            app, TodoDatabase::class.java, "todo_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTodoRepository(db: TodoDatabase): TodoRepository {
+        return TodoRepositoryImpl(db.dao)
+    }
+}
