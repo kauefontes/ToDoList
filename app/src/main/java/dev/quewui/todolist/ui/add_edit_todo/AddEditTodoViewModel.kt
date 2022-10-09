35fb4606cfb4bf53d944e59bdb8ab033cd976b28
Kaue Pereira <kauefontes@outlook.com>
2022-10-08 22:20:35 -0400

    Added created ViewModel to the Add/Edit todo screen

---
+++ app/src/main/java/dev/quewui/todolist/ui/add_edit_todo/AddEditTodoViewModel.kt
@@ -0,0 +1,82 @@
+package dev.quewui.todolist.ui.add_edit_todo
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.quewui.todolist.data.Todo
+import dev.quewui.todolist.data.TodoRepository
+import dev.quewui.todolist.util.Routes
+import dev.quewui.todolist.util.UiEvent
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AddEditTodoViewModel @Inject constructor(
+    private val repository: TodoRepository,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+    var todo by mutableStateOf<Todo?>(null)
+        private set
+
+    var title by mutableStateOf("")
+        private set
+
+    var description by mutableStateOf("")
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    init {
+        val todoId = savedStateHandle.get<Int>("todoId")
+        if (todoId != -1) {
+            viewModelScope.launch {
+                if (todoId != null) {
+                    repository.getTodoById(todoId)?.let { todo ->
+                        title = todo.title
+                        description = todo.description ?: ""
+                        this@AddEditTodoViewModel.todo = todo
+                    }
+                }
+            }
+        }
+    }
+
+    fun onEvent(event: AddEditTodoEvent) {
+        when (event) {
+            is AddEditTodoEvent.OnTitleChange -> {
+                title = event.title
+            }
+            is AddEditTodoEvent.OnDescriptionChange -> {
+                description = event.description
+            }
+            is AddEditTodoEvent.OnSaveTodoClick -> {
+                viewModelScope.launch {
+                    if (title.isNotBlank()) {
+                        sendUiEvent(UiEvent.ShowSnackbar("The title cant be empty!"))
+                        return@launch
+                    }
+                    repository.insertTodo(
+                        Todo(
+                            title, description, todo?.done ?: false,
+                            id = todo?.id
+                        )
+                    )
+                    sendUiEvent(UiEvent.PopBackStack)
+                }
+            }
+        }
+    }
+
+    private fun sendUiEvent(event: UiEvent) {
+        viewModelScope.launch {
+            _uiEvent.send(UiEvent.Navigate(Routes.ADD_EDIT_TODO))
+        }
+    }
+}
